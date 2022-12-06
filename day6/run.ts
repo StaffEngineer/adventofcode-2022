7e01fe2089bd9d646f7e273e9f10495ea6ada8e3
Dima <dmytro.rets@gamesys.co.uk>
2022-12-06 08:01:03 +0000

    solve day 6 (part1)

---
+++ day6/run.ts
@@ -0,0 +1,26 @@
+import * as fs from 'fs'
+import * as _ from 'lodash'
+
+const input = fs.readFileSync('./input.txt', 'utf-8')
+let l = 0
+let r = 4
+
+const store = new Map()
+let i = 0
+while (i < r) {
+    store.set(input[i], 1 + (store.get(input[i]) ?? 0))
+    i++
+}
+while (r < input.length) {
+    if (store.size === 4) break
+    store.set(input[r], 1 + (store.get(input[r]) ?? 0))
+    const left = store.get(input[l])
+    if (left === 1) {
+        store.delete(input[l])
+    } else {
+        store.set(input[l], left - 1)
+    }
+    l++
+    r++
+}
+console.log(r)
