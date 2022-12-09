94e4a4f1cf65cedd49aa82166aa4d2a5b371c229
Dima <dmytro.rets@gamesys.co.uk>
2022-12-09 12:06:35 +0000

    solve day 9 (part2)

--- day9/run.ts
+++ day9/run.ts
@@ -1,5 +1,6 @@
 import * as fs from 'fs'
 import * as _ from 'lodash'
+import { head } from 'lodash'
 
 const input = fs.readFileSync('./input.txt', 'utf-8')
 
@@ -11,8 +12,9 @@ const directions = new Map()
 
 const visited = new Set()
     .add([0, 0].toString())
-let [headX, headY] = [0, 0]
-let [tailX, tailY] = [0, 0]
+const rope: [number, number][] = []
+const N = 10
+for (let i = 0; i < N; i++) rope.push([0, 0])
 
 for (const command of input.split('\n')) {
     const [directionInput, stepsInput] = command.split(' ')
@@ -20,46 +22,61 @@ for (const command of input.split('\n')) {
     const [x, y] = directions.get(directionInput)
     let i = 0
     while (i < steps) {
-        i++
-        headX += x
-        headY += y
-        // the same position
-        if (headX === tailX && headY === tailY) continue
-        // in the same row or column
-        if (headX === tailX || headY === tailY) {
-            if (Math.abs(headX - tailX) === 1 || Math.abs(headY - tailY) === 1) continue
-            const directionX = headX - tailX
-            const directionY = headY - tailY
-            if (directionX < 0) {
-                tailX -= 1
-            } else if (directionX > 0) {
-                tailX += 1
-            }
-            if (directionY < 0) {
-                tailY -= 1
-            } else if (directionY > 0) {
-                tailY += 1
-            }
-        } 
-        // diagonal
-        if (headX !== tailX && headY !== tailY) {
-            // close enough - do nothing
-            if (Math.abs(headX - tailX) === 1 && Math.abs(headY - tailY) === 1) continue
-            const directionX = headX - tailX
-            const directionY = headY - tailY
-            if (directionX < 0) {
-                tailX -= 1
-            } else {
-                tailX += 1
-            }
-            if (directionY < 0) {
-                tailY -= 1
-            } else {
-                tailY += 1
-            }
+        rope[N-1][0] += x
+        rope[N-1][1] += y
+        let j = N - 1
+        while (j > 0) {
+            const knotHead = rope[j]
+            const knotTail = rope[j-1]
+            const newTail = getNewPosition(knotHead, knotTail)
+            if (newTail[0] === knotTail[0] && newTail[1] === knotTail[1]) break
+            rope[j-1][0] = newTail[0]
+            rope[j-1][1] = newTail[1]
+            if (j === 1) visited.add(newTail.toString())
+            j--
         }
-        visited.add([tailX, tailY].toString())
+        i++
     }
 }
 
 console.log(visited.size)
+
+function getNewPosition(head: [number, number], tail: [number, number]): [number, number] {
+    let [headX, headY] = head
+    let [tailX, tailY] = tail
+    if (headX === tailX && headY === tailY) return [tailX, tailY]
+    // in the same row or column
+    if (headX === tailX || headY === tailY) {
+        if (Math.abs(headX - tailX) === 1 || Math.abs(headY - tailY) === 1) return [tailX, tailY]
+        const directionX = headX - tailX
+        const directionY = headY - tailY
+        if (directionX < 0) {
+            tailX -= 1
+        } else if (directionX > 0) {
+            tailX += 1
+        }
+        if (directionY < 0) {
+            tailY -= 1
+        } else if (directionY > 0) {
+            tailY += 1
+        }
+    } 
+    // diagonal
+    if (headX !== tailX && headY !== tailY) {
+        // close enough - do nothing
+        if (Math.abs(headX - tailX) === 1 && Math.abs(headY - tailY) === 1) return [tailX, tailY]
+        const directionX = headX - tailX
+        const directionY = headY - tailY
+        if (directionX < 0) {
+            tailX -= 1
+        } else {
+            tailX += 1
+        }
+        if (directionY < 0) {
+            tailY -= 1
+        } else {
+            tailY += 1
+        }
+    }
+    return [tailX, tailY]
+}
