77ad4c14486922bcb7dd6257b73de7502863026c
Dima <dmytro.rets@gamesys.co.uk>
2022-12-15 16:15:40 +0000

    solve day 15(part2)

--- day15/run.ts
+++ day15/run.ts
@@ -22,22 +22,49 @@ function setDistance(sensor: Sensor) {
 
 sensors.forEach(setDistance)
 
-let y = 2000000
-let cells = new Set()
-for (let { sensor, distance, beacon } of sensors) {
-    const [r,c] = sensor
-    if ((r-distance) < y && y < (r+distance)) {
-        const diff = y-r
-        const steps = distance - Math.abs(diff)
-        cells.add([r+diff, c].toString())
-        let i = 1
-        while (i <= steps) {
-            cells.add([r+diff, c+i].toString())
-            cells.add([r+diff, c-i].toString())
+let state = new Map()
+
+const l = 0
+const h = 4000000
+const N = 4000000
+
+function merge(intervals: number[][]): number[][] {
+    intervals.sort((a,b)=> a[0] - b[0])
+    const res: number[][] = []
+    for (const interval of intervals) {
+        if (res.length > 0 && res[res.length-1][1]+1 >= interval[0]) {
+            res[res.length-1][1] = Math.max(interval[1], res[res.length-1][1])
+        } else {
+            res.push(interval)
+        }
+    }
+    return res
+};
+
+function run() {
+    for (let { sensor, distance } of sensors) {
+        const [r,c] = sensor
+        let i = -distance
+        let l = 0
+        while (i <= 0) {
+            state.set(r+i, merge([...(state.get(r+i) ?? []), [c-l, c+l]]))
+            l++
+            i++
+        }
+        l-=2
+        while (i <= distance) {
+            state.set(r+i, merge([...(state.get(r+i) ?? []), [c-l, c+l]]))
+            l--
             i++
         }
     }
-    cells.delete(beacon.toString())
 }
 
-console.log(cells.size)
+run()
+
+for (let i = l; i <= h; i++) {
+    const interval = state.get(i)
+    if (interval.length === 2) {
+        console.log((interval[0][1]+1)*N+i)
+    }
+}
